9d127c7d340ebd1f242811790627f14195031a8e
aleryhu <111310786+aleryhu@users.noreply.github.com>
2022-12-14 00:59:50 +0100

    Updated

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const express = require('express');
 const app = express();
 app.use(express.static('./Public'));
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.post('/data', (req, res) => {
   const { payload } = req.body;
-  console.log(req);
-  res.status(201).json({sucess:true});
+  console.log(payload);
+  res.status(201).json({ sucess: true });
 });
 
 const port = 5000;
